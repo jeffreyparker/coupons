ac0c608094b7f1ad7da5c9c8f21db7e319c81035
Jeffrey Parker <jeffreyparker@gmail.com>
2023-04-10 16:38:12 -0400

    Update amex selector

--- amex.js
+++ amex.js
@@ -4,7 +4,7 @@
 // Paste snippet below into console and hit enter
 // Wait. Eventually it'll click all the buttons it can find, reload the page and redo step 3 if more offers.
 // Find all the "Add to Card" buttons on the page
-var offerButtons = Array.from(document.getElementsByClassName("offer-cta axp-offers__global__fluid___15Bk5 css-1duq00z")).filter(btn => btn.title == "Add to Card");
+var offerButtons = Array.from(document.getElementsByClassName("offer-cta axp-offers__global__fluid___15Bk5 css-261oxr")).filter(btn => btn.title == "Add to Card");
 var index;
 for (index = 0; index < offerButtons.length; ++index) {
     console.log("Clicking offer button");
